b3eda1fc624ade428bdf56ff062cb321121dfdce
Алексей Бельченков <u608110@gmail.com>
2022-08-23 19:00:33 +0400

    Get & Create Tickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -8,9 +8,19 @@ const Ticket = require('../models/ticketModel');
 // @route GET /api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(StatusCodes.UNAUTHORIZED);
+        throw new Error('User not found');
+    }
+
+    const tickets = await Ticket.find({ user: req.user.id });
+
     res.status(StatusCodes.OK)
         .json({
-            message: 'getTickets'
+            status: true,
+            tickets,
         })
 });
 
@@ -18,9 +28,31 @@ const getTickets = asyncHandler(async (req, res) => {
 // @route POST /api/tickets
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
+    const { product, description } = req.body;
+
+    if (!product || !description) {
+        res.status(StatusCodes.UNPROCESSABLE_ENTITY);
+        throw new Error('Please add a product and description');
+    }
+
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(StatusCodes.UNAUTHORIZED);
+        throw new Error('User not found');
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: 'new'
+    })
+
     res.status(StatusCodes.CREATED)
         .json({
-            message: 'createTickets'
+            status: true,
+            ticket,
         })
 });
 

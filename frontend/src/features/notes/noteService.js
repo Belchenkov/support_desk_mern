47806ab3c26fadebb5aa804bedd346405b2f611c
Алексей Бельченков <u608110@gmail.com>
2022-10-04 11:38:40 +0400

    Fetch notes through redux

---
+++ frontend/src/features/notes/noteService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_URL = '/api/tickets/';
+
+const getNotes = async (ticketId, token) => {
+    const response = await axios.get(API_URL + ticketId + '/notes', {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    if (response?.data.status) {
+        return response.data.tickets;
+    }
+};
+
+const createNote = async (ticketData, token) => {
+    const response = await axios.post(API_URL, ticketData, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    if (response?.data.status) {
+        return response.data;
+    }
+};
+
+const noteService = {
+    createNote,
+    getNotes,
+};
+
+export default noteService;
---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,70 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+import noteService from './noteService';
+
+const initialState = {
+    notes: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+};
+
+export const noteSlice = createSlice({
+    name: 'note',
+    initialState,
+    reducers: {
+        reset: (state) => initialState,
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(createNote.pending, state => {
+                state.isLoading = true;
+            })
+            .addCase(createNote.fulfilled, state => {
+                state.isLoading = false;
+                state.isSuccess = true;
+            })
+            .addCase(createNote.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            })
+            .addCase(getNotes.pending, state => {
+                state.isLoading = true;
+            })
+            .addCase(getNotes.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.notes = action.payload;
+            })
+            .addCase(getNotes.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            })
+    }
+});
+
+export const getNotes = createAsyncThunk('notes/getAll', async (_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+
+        return await noteService.getNotes(token);
+    } catch (err) {
+        return thunkAPI.rejectWithValue(err?.response?.data?.message || err.message || err.toString());
+    }
+});
+
+export const createNote = createAsyncThunk('notes/create', async (noteData, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+
+        return await noteService.createNote(noteData, token);
+    } catch (err) {
+        return thunkAPI.rejectWithValue(err?.response?.data?.message || err.message || err.toString());
+    }
+});
+
+export const { reset } = noteSlice.actions;
+export default noteSlice.reducer;
